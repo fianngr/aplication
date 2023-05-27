5ae94db1cf722762df06242b953b68ff997e352a
alfian <alfiannugroho95@gmail.com>
2023-05-27 21:43:15 +0700

    Menambahkan content, css, dan assets serta menambahkan javascipt

--- application/views/templates/content.php
+++ application/views/templates/content.php
@@ -1,8 +1,52 @@
-<div class="container-content">    
+<section id="home">
+  <div class="container-content">    
     <div class="pxtext">
-        <span class="border">
-          MAY CAKE
-        </span>
+      <span class="border">
+        Selamat Datang di MAY CAKE
+      </span>
+      <div class="container-content-home">
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus laboriosam dolorum, error odio alias nemo beatae illum odit sint labore nostrum, temporibus maiores dolor! Laudantium assumenda nihil cupiditate hic inventore.</p>
+        <img src="">
         <p><a href="#">Beli Sekarang</a></p>
+      </div>
     </div>
-</div>
+  </div>
+</section>
+
+<section class="preview">
+      <h2 align="center">Best Sellers</h2>
+      <div class="bestseller">
+        <div class="content-product">  
+          <figure class="item">
+            <a href="#">
+              <img src="<?= base_url()?>assets/images/product/TIM BG.jpg" alt="" width="100%" />
+              <figcaption>Love Cake</figcaption>
+            </a>
+          </figure>
+        </div>
+        <div class="content-product">
+          <figure class="item">
+            <a href="#">
+              <img src="<?= base_url()?>assets/images/product/TIM.jpg" alt="" width="100%"  />
+              <figcaption>Strobery Cake</figcaption>
+            </a>
+          </figure>
+        </div>
+        <div class="content-product">
+          <figure class="item">
+            <a href="#">
+              <img src="<?= base_url()?>assets/images/product/kartun.jpg" width="100%" height="80%"/>
+              <figcaption>Doraemon</figcaption>
+            </a>
+          </figure>
+        </div>
+        <div class="content-product">
+          <figure class="item">
+            <a href="#">
+              <img src="<?= base_url()?>assets/images/product/kartunspg.jpg" alt="" width="100%" />
+              <figcaption>Spongspob Cake</figcaption>
+            </a>
+          </figure>
+        </div>
+      </div>
+    </section>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -10,8 +10,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <title><?= $judul ?></title>
     <link rel="stylesheet" href="<?= base_url()?>assets/css/templates/style.css">
 	<link href="<?= base_url()?>assets/boostrap/css/bootstrap.min.css" rel="stylesheet">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-	<script src="<?=base_url()?>assets/js/script.js"></script>
+	<script src="https://kit.fontawesome.com/b5f72843f3.js" crossorigin="anonymous"></script>
+	
 </head>
 <body>
 	<div class="preloader">
@@ -20,10 +20,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		</div>
 	</div>	
 	<div class="container" style="height: 1200px;">
-		<div class="container">
+		<div class="container" >
 			<nav class="navbar navbar-expand-lg bg-body-tertiary mb-5">
-				<div class="container-fluid">
-					<a class="navbar-brand" href="#">Maycake</a>
+				<div class="container-fluid mt-1 pt-1">
+					<a class="navbar-brand" href="<?= base_url()?>">Maycake</a>
 					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
 						<span class="navbar-toggler-icon"></span>
 					</button>
@@ -31,16 +31,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					<div class="collapse navbar-collapse" id="navbarNav">
 						<ul class="nav nav-pills nav-fill ms-auto">
 							<li class="nav-item">
-								<a class="nav-link active" aria-current="page" href="#">Home</a>
+								<a class="nav-link navlink active" aria-current="page" href="<?= base_url()?>home">Home</a>
 							</li>
 							<li class="nav-item">
-								<a class="nav-link" href="#">Product</a>
+								<a class="nav-link navlink" href="#">Product</a>
 							</li>
 							<li class="nav-item">
-								<a class="nav-link" href="#">Contact Us</a>
+								<a class="nav-link navlink" href="#">Contact Us</a>
 							</li>
 							<li class="nav-item">
-								<a class="nav-link " href="#" tabindex="-1" aria-disabled="true">Our Team</a>
+								<a class="nav-link navlink" href="#" tabindex="-1" aria-disabled="true"><i class="fa-solid fa-cart-shopping"></i>Cart</a>
 							</li>
 						</ul>
 					</div>
